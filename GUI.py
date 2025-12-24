7dfe7a80b8ad8270f72ef6964065d8ca739afde8
NoahB6JS <noah.js.b6@gmail.com>
2025-12-24 15:34:43 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,25 +1,49 @@
 import customtkinter
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("400x150")
-        self.label = customtkinter.CTkLabel(self, text="Speak to your tutor:", fg_color="transparent")
-        
+class MyFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
+        self.label = customtkinter.CTkLabel(self)
+        self.label.grid(row=0, column=0, padx=20)
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="enter here:", width = 360, height=120)
         self.entry.pack(padx=20, pady=20)
 
         self.button = customtkinter.CTkButton(
             self,
-            text="my button",
+            text="Ask",
             command=self.button_callback
         )
         self.button.pack(padx=20, pady=20)
 
     def button_callback(self):
-        text = self.entry.get()
-        print(text)
+        prompt = self.entry.get()
+        print(prompt)
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x600")
+
+        self.label = customtkinter.CTkLabel(
+            self,
+            text="Computer Science helper",
+            font=('arial', 30)
+        )
+        self.label.pack(padx=20, pady=20)
+
+        self.label1 = customtkinter.CTkLabel(
+            self,
+            text="Speak to your tutor:",
+            font=('arial', 15)
+        )
+        self.label1.pack(padx=20, pady=10)
+
+        self.frame = MyFrame(self)
+        self.frame.pack(fill="both", expand=True, padx=20, pady=20)
+
+        
+
 
 app = App()
 app.mainloop()
